b08710014437faf1fe3d2a491619c2efb0b13bfb
at3158 <tk290@tkfood.com.tw>
2018-09-26 10:50:19 +0800

    FINISH

--- TKGAFFAIRS/FrmBOXMEALORDER.Designer.cs
+++ TKGAFFAIRS/FrmBOXMEALORDER.Designer.cs
@@ -42,6 +42,17 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -189,9 +200,20 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 7);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 2, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 2, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -245,6 +267,141 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button3.Location = new System.Drawing.Point(122, 122);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 36);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "訂餐-中餐-葷";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button6.Location = new System.Drawing.Point(242, 122);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 36);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "訂餐-中餐-素";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button4.Location = new System.Drawing.Point(122, 162);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 36);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "訂餐-晚餐-葷";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button7.Location = new System.Drawing.Point(242, 162);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 36);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "訂餐-晚餐-素";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(122, 242);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 36);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "取消-中餐-葷";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button5.Location = new System.Drawing.Point(242, 242);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 36);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "取消-中餐-素";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button8.Location = new System.Drawing.Point(122, 282);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(116, 36);
+            this.button8.TabIndex = 24;
+            this.button8.Text = "取消-晚餐-葷";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button9.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button9.Location = new System.Drawing.Point(242, 282);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(116, 36);
+            this.button9.TabIndex = 26;
+            this.button9.Text = "取消-晚餐-素";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(123, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 27);
+            this.textBox1.TabIndex = 27;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(2, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "工號";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(243, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 27);
+            this.textBox2.TabIndex = 29;
+            // 
             // FrmBOXMEALORDER
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
@@ -290,5 +447,16 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- TKGAFFAIRS/FrmBOXMEALORDER.cs
+++ TKGAFFAIRS/FrmBOXMEALORDER.cs
@@ -74,6 +74,8 @@ namespace TKGAFFAIRS
             timer1.Interval = 1000;
             timer1.Start();
         }
+      
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Text = DateTime.Now.ToString();
@@ -139,12 +141,354 @@ namespace TKGAFFAIRS
             }
         }
 
+        public void ORDERAdd(string Meal, string Dish, string OrderBoxed)
+        {
+            try
+            {
+
+                InsertsbSql.Clear();
+                sbSql.Clear();
+                //ADD COPTC
+
+                if (Meal.Equals("10+20"))
+                {
+                    DataSet ds1 = new DataSet();
+                    connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+                    sbSqlQuery.Clear();
+
+                    sbSql.AppendFormat(@"SELECT [SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM],[EATNUM] FROM [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] WHERE  CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  [MEAL]='{1}'  ", EmployeeID, "10");
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds1.Clear();
+                    adapter.Fill(ds1, "TEMPds1");
+                    sqlConn.Close();
+
+                    if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                    {
+                        //InsertsbSql.AppendFormat(" DELETE [TKBOXEDMEAL].[dbo].[EMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  ([MEAL]='10' OR [MEAL]='20') AND [EATNUM]=0", EmployeeID, Meal);
+                        Meal = "10";
+                        InsertsbSql.Append(" ");
+                        InsertsbSql.AppendFormat(" INSERT INTO  [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] ([SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM]) VALUES ('{0}','{1}','{2}','{3}',GETDATE(),'{4}','{5}',1) ", DateTime.Now.ToString("yyyyMMddHHmmss"), EmployeeID, Name, CardNo, Meal, Dish);
+                    }
+
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+                    sbSqlQuery.Clear();
+
+                    sbSql.AppendFormat(@"SELECT [SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM],[EATNUM] FROM [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  [MEAL]='{1}'  ", EmployeeID, "20");
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds1.Clear();
+                    adapter.Fill(ds1, "TEMPds1");
+                    sqlConn.Close();
+                    if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                    {
+                        Meal = "20";
+                        InsertsbSql.Append(" ");
+                        InsertsbSql.AppendFormat(" INSERT INTO  [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] ([SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM]) VALUES ('{0}','{1}','{2}','{3}',GETDATE(),'{4}','{5}',1) ", DateTime.Now.ToString("yyyyMMddHHmmss"), EmployeeID, Name, CardNo, Meal, Dish);
+                    }
+
+                }
+                else
+                {
+                    DataSet ds1 = new DataSet();
+                    connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+                    sbSqlQuery.Clear();
+
+                    sbSql.AppendFormat(@"SELECT [SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM],[EATNUM] FROM [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  [MEAL]='{1}' ", EmployeeID, Meal);
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds1.Clear();
+                    adapter.Fill(ds1, "TEMPds1");
+                    if (ds1.Tables["TEMPds1"].Rows.Count > 0)
+                    {
+                        Name = ds1.Tables["TEMPds1"].Rows[0][2].ToString();
+                    }
+                    sqlConn.Close();
+
+                    if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                    {
+                        InsertsbSql.Append(" ");
+                        //InsertsbSql.AppendFormat(" DELETE [TKBOXEDMEAL].[dbo].[EMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  [MEAL]='{1}' AND [EATNUM]=0 ", EmployeeID, Meal);
+                        InsertsbSql.AppendFormat(" INSERT INTO  [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] ([SERNO],[ID],[NAME],[CARDNO],[DATE],[MEAL],[DISH],[NUM]) VALUES ('{0}','{1}','{2}','{3}',GETDATE(),'{4}','{5}',1) ", DateTime.Now.ToString("yyyyMMddHHmmss"), EmployeeID, Name, CardNo, Meal, Dish);
+                    }
+                    else
+                    {
+                        //AutoClosingMessageBox.Show("已經訂過餐了!!", "TITLE", messagetime);
+                        SHOWMESSAGE(Name + "已經訂過餐了!!!!");
+                    }
+
+                }
+
+
+                if (!string.IsNullOrEmpty(InsertsbSql.ToString()))
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sqlConn.Close();
+                    sqlConn.Open();
+                    tran = sqlConn.BeginTransaction();
+                    cmd.Connection = sqlConn;
+                    cmd.CommandTimeout = 60;
+                    cmd.CommandText = InsertsbSql.ToString();
+                    cmd.Transaction = tran;
+                    result = cmd.ExecuteNonQuery();
+                    if (result == 0)
+                    {
+                        tran.Rollback();    //交易取消
+                        SHOWMESSAGE(Name + " 訂餐失敗!!");
+
+                    }
+                    else
+                    {
+                        tran.Commit();      //執行交易  
+                        SHOWMESSAGE(Name + " 訂餐成功!!" + " 訂了: " + OrderBoxed.ToString());
+                    }
+
+                    sqlConn.Close();
+                }
+         
+
+               
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+            
+        }
+
+        public void SHOWMESSAGE(String mess)
+        {
+            MessageBox.Show(mess);
+        }
+
+        public void OrderCanel(string Meal, string Dish, string OrderBoxed)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+                //ADD COPTC
+
+                if (Meal.Equals("10+20"))
+                {
+                    sbSql.AppendFormat(" DELETE [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  ([MEAL]='10' OR [MEAL]='20') AND [DISH]='{1}' ", EmployeeID, Dish);
+                }
+                else
+                {
+                    sbSql.Append(" ");
+                    sbSql.AppendFormat(" DELETE [TKBOXEDMEAL].[dbo].[LOCALEMPORDER] WHERE CONVERT(varchar(100),[DATE], 112)=CONVERT(varchar(100),GETDATE(), 112) AND [ID]='{0}' AND  [MEAL]='{1}' AND [DISH]='{2}' ", EmployeeID, Meal, Dish);
+                }
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+
+                    SHOWMESSAGE(Name + " 取消訂餐失敗!!");
+
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+                    SHOWMESSAGE(Name + " 取消訂餐成功!!" + " 您取消了: " + OrderBoxed.ToString());
+
+                }
+
+                sqlConn.Close();
+                Search();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+            
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SearchEmplyee();
+        }
+
+        public void SearchEmplyee()
+        {
+            try
+            {
+
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"SELECT TOP 1  [EmployeeID],[CardNo],[Name] FROM [TKBOXEDMEAL].[dbo].[VEMPLOYEE] WHERE [EmployeeID]='{0}' OR [CardNo]='{0}'", textBox1.Text.ToString());
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                sqlConn.Open();
+                ds1.Clear();
+                adapter.Fill(ds1, "TEMPds1");
+                sqlConn.Close();
+
+                if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                {
+
+                    //SHOWMESSAGE("沒有此員工!!");
+
+                    //textBox1.Text = null;
+                    //textBox2.Text = null;
+
+
+                }
+                else
+                {
+                    EmployeeID = ds1.Tables["TEMPds1"].Rows[0][0].ToString();
+                    CardNo = ds1.Tables["TEMPds1"].Rows[0][1].ToString();
+                    Name = ds1.Tables["TEMPds1"].Rows[0][2].ToString();
+
+                    textBox2.Text = ds1.Tables["TEMPds1"].Rows[0][2].ToString();
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
         #endregion
 
 
         #region BUTTON
         private void button1_Click(object sender, EventArgs e)
         {
+            Search();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            Meal = "10";
+            Dish = "1";
+            EmployeeID = textBox1.Text;
+
+            ORDERAdd(Meal, Dish, OrderBoxed);
+
+            Search();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Meal = "10";
+            Dish = "2";
+            EmployeeID = textBox1.Text;
+
+            ORDERAdd(Meal, Dish, OrderBoxed);
+
+            Search();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Meal = "20";
+            Dish = "1";
+            EmployeeID = textBox1.Text;
+
+            ORDERAdd(Meal, Dish, OrderBoxed);
+
+            Search();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Meal = "20";
+            Dish = "2";
+            EmployeeID = textBox1.Text;
+
+            ORDERAdd(Meal, Dish, OrderBoxed);
+            Search();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Meal = "10";
+            Dish = "1";
+            EmployeeID = textBox1.Text;
+
+            OrderCanel(Meal, Dish, OrderBoxed);
+
+            Search();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Meal = "10";
+            Dish = "2";
+            EmployeeID = textBox1.Text;
+
+            OrderCanel(Meal, Dish, OrderBoxed);
+
+            Search();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Meal = "20";
+            Dish = "1";
+            EmployeeID = textBox1.Text;
+
+            OrderCanel(Meal, Dish, OrderBoxed);
+            Search();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Meal = "20";
+            Dish = "2";
+            EmployeeID = textBox1.Text;
+
+            OrderCanel(Meal, Dish, OrderBoxed);
             Search();
         }
 
