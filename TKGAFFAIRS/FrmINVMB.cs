5fcfd061f7a115e1b5e77e9dd0989def26f1a43d
at3158 <tk290@tkfood.com.tw>
2018-11-28 10:07:11 +0800

    FrmINVMB

---
+++ TKGAFFAIRS/FrmINVMB.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+using FastReport;
+using FastReport.Data;
+
+namespace TKGAFFAIRS
+{
+    public partial class FrmINVMB : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter4 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder4 = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        DataSet ds4 = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        string EDITID;
+        string STATUS=null;
+        int result;
+        Thread TD;
+
+        public FrmINVMB()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void SEARCHINVMB()
+        {
+            ds.Clear();
+
+            StringBuilder NAME = new StringBuilder();
+            
+
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                NAME.AppendFormat(@" WHERE ( [MB001] LIKE '%{0}%' OR [MB002] LIKE '%{0}%')", textBox1.Text);
+            }
+
+          
+
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  SELECT [MB001] AS '品號',[MB002] AS '品名',[MB003] AS '規格'");
+                sbSql.AppendFormat(@"  FROM [TKGAFFAIRS].[dbo].[INVMB]");
+                sbSql.AppendFormat(@"  {0}", NAME.ToString());
+                sbSql.AppendFormat(@"  ORDER BY [MB001] ");
+                sbSql.AppendFormat(@"  ");
+
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "TEMPds1");
+                sqlConn.Close();
+
+
+                if (ds.Tables["TEMPds1"].Rows.Count == 0)
+                {
+                    dataGridView1.DataSource = null;
+                }
+                else
+                {
+                    if (ds.Tables["TEMPds1"].Rows.Count >= 1)
+                    {
+                        dataGridView1.DataSource = ds.Tables["TEMPds1"];
+                        dataGridView1.AutoResizeColumns();
+
+
+                    }
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SEARCHINVMB();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
