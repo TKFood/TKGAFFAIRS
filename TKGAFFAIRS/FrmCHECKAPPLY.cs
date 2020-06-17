af48949e27ed0ff752f6043956053d9bd947a0e6
at3158 <tk290@tkfood.com.tw>
2020-06-17 11:19:29 +0800

    UPDATETUOFHREngFrm001HREngFrm001BakTime

--- TKGAFFAIRS/FrmCHECKAPPLY.cs
+++ TKGAFFAIRS/FrmCHECKAPPLY.cs
@@ -81,11 +81,9 @@ namespace TKGAFFAIRS
                 {
                     query.AppendFormat(@"  ");
                 }
-              
-                sbSql.AppendFormat(@"  SELECT [HREngFrm001Name] AS '姓名',[HREngFrm001Dpt] AS '部門',[HREngFrm001Date] AS '日期',[HREngFrm001TITLE] AS '職稱',[HREngFrm001Agent] AS '代理人' ");
-                sbSql.AppendFormat(@"  ,[HREngFrm001Transp] AS '交通工具',[HREngFrm001Location] AS '外出地點',[HREngFrm001Cause] AS '外出原因',[HREngFrm001DefOutTime] AS '預計外出時間',[HREngFrm001OutTime] AS '實際外出時間' ");
-                sbSql.AppendFormat(@"  ,[HREngFrm001DefBakTime] AS '預計返廠時間',[HREngFrm001BakTime] AS '實際返廠時間',[HREngFrm001UsrDpt] AS '申請人部門',[HREngFrm001User] AS '申請人姓名'");
-                sbSql.AppendFormat(@"  ,[TaskId],[HREngFrm001SN]");
+
+                sbSql.AppendFormat(@"  SELECT [HREngFrm001SN] AS '表單編號	',[HREngFrm001Date] AS '日期',[HREngFrm001User] AS '申請人',[HREngFrm001Name] AS '姓名',[HREngFrm001UsrDpt] AS '部門',[HREngFrm001Rank] AS '職級',[HREngFrm001TITLE] AS '職稱',[HREngFrm001Agent] AS '代理人',[HREngFrm001Transp] AS '交通工具',[HREngFrm001Location] AS '外出地點'	,[HREngFrm001Cause] AS '外出原因',[HREngFrm001DefOutTime] AS '預計外出時間',[HREngFrm001OutTime] AS '實際外出時間',[HREngFrm001DefBakTime] AS '預計返廠時間',[HREngFrm001BakTime] AS '實際返廠時間'	");
+                sbSql.AppendFormat(@"  ,[TaskId]");
                 sbSql.AppendFormat(@"  FROM [TKGAFFAIRS].[dbo].[HREngFrm001]");
                 sbSql.AppendFormat(@"  WHERE [HREngFrm001Date]>='{0}' AND [HREngFrm001Date]<='{1}'",dateTimePicker1.Value.ToString("yyyy/MM/dd"), dateTimePicker2.Value.ToString("yyyy/MM/dd"));
                 sbSql.AppendFormat(@"  {0}",query.ToString());
